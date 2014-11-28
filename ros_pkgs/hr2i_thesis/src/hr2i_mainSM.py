df4556def219ab6b0a4f91c46d92770db8ed297d
Gerard Canal <grar.knal@gmail.com>
2014-11-28 22:24:30 +0100

    WOrking on StateMachines...

--- ros_pkgs/hr2i_thesis/src/hr2i_mainSM.py
+++ ros_pkgs/hr2i_thesis/src/hr2i_mainSM.py
@@ -1,1 +1,48 @@
 #!/usr/bin/env python
+import rospy
+import smach_ros
+
+from smach import StateMachine
+from nao_smach_utils.check_nodes import CheckNodesState
+from nao_smach_utils.home_onoff import HomeOff_SM, HomeOn_SM
+from wifibot_smach.wifibot_goto_state import GoToPositionState
+from hr2i_smach_states import WaitForGestureRecognitionState
+
+class HR2I_SM(StateMachine):
+    def __init__(self):
+        StateMachine.__init__(self, outcomes=['succeeded', 'preempted', 'aborted'])
+
+        with self:
+            StateMachine.add('WAIT_FOR_GR', WaitForGestureRecognitionState(),
+                             transitions={'succeeded':''}, remapping={'out_ground_point': 'ground_point'}) # FIXME maybe timeout and say somethinga gain
+            StateMachine.add('MOVE_TO_POINTING_PLACE', GoToPositionState())
+
+if __name__ == "__main__":
+    rospy.init_node('HR2I_main_pipeline_node')
+
+    TOPIC_LIST_NAMES = ['/recognized_gesture', '/kinect2_blobs']
+    SERVICES_LIST_NAMES = ['/cmd_pose_srv', '/wb_move_to_srv']
+    ACTION_LIST_NAMES = ['/speech','/joint_angles_action']
+    PARAMS_LIST_NAMES = []
+
+    # Create a SMACH state machine
+    sm = StateMachine(outcomes=['succeeded', 'aborted', 'preempted'])
+
+    with sm:
+        StateMachine.add('CHECK_NODES', CheckNodesState(TOPIC_LIST_NAMES, SERVICES_LIST_NAMES, ACTION_LIST_NAMES, PARAMS_LIST_NAMES),
+                         transitions={'succeeded':'HOME_ON','aborted':'aborted'})
+        
+        StateMachine.add('HOME_ON', HomeOn_SM(startPose='Crouch'), transitions={'succeeded': 'HR2I_SM'})
+        StateMachine.add('HR2I_SM', HR2I_SM(), transitions={'succeeded': 'HOME_OFF'})
+        StateMachine.add('HOME_OFF', HomeOff_SM(), transitions={'succeeded': 'succeeded'})
+
+
+    sis = smach_ros.IntrospectionServer('hr2i_iserver', sm, '/HR2I_ROOT')
+    sis.start()
+
+    # Execute the state machine
+    outcome = sm.execute()
+
+    # Wait for ctrl-c to stop the application
+    rospy.spin()
+    sis.stop()
--- ros_pkgs/hr2i_thesis/src/hr2i_smach_states.py
+++ ros_pkgs/hr2i_thesis/src/hr2i_smach_states.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 import rospy
-from smach import State
+from smach import State, StateMachine
 
 from hr2i_thesis.msg import GestureRecognitionResult
+from nao_smach_utils.execute_choregraphe_behavior_state import ExecuteBehavior
+from nao_smach_utils.tts_state import SpeechState
 
 class WaitForGestureRecognitionState(State):
     def __init__(self, gesture_topic='/recognized_gesture', timeout=None):
-        State.__init__(self, outcomes=['pointat_recognized', 'hello_recognized', 'failed_recognition'], output_keys=['ground_point'])
+        State.__init__(self, outcomes=['pointat_recognized', 'hello_recognized', 'failed_recognition'],
+                       output_keys=['out_ground_point'])
         self._topic = gesture_topic
         self._timeout = rospy.Duration(timeout) if timeout else None
         self._gesture_received = None
@@ -32,3 +35,41 @@ class WaitForGestureRecognitionState(State):
         else:
             ud.ground_point = None
             return 'failed_recognition' # Timeouted
+
+class ReleaseNAOFromWifiBotState(ExecuteBehavior):
+    def __init__(self):
+        ExecuteBehavior.__init__(self, 'release_nao_from_wb')
+
+class NaoSayHello(StateMachine):
+    def __init__(self, text):
+        StateMachine.__init__(self, outcomes=['succeeded', 'aborted'])
+
+        with self:
+            StateMachine.add('SAY_HELLO', SpeechState)
+
+class WaitForObjectBlobsState(State):
+    def __init__(self, blob_topic='/kinect2_blobs', timeout=3):
+        State.__init__(self, outcomes=['succeeded', 'aborted'], output_keys=['blobsFIXME'])
+        self._topic = blob_topic
+        self._timeout = rospy.Duration(timeout)
+        self._blob_received = None
+
+    def gesture_cb(self, data):
+        self._blob_received = data
+
+    def execute(self, ud):
+        subs = rospy.Subscriber(self._topic, 'FIXME BlobResult', self.blob_cb)
+        startT = rospy.Time.now()
+        timeout = False
+        while not (timeout or (self._gesture_received)):
+            timeout = (rospy.Time.now()-startT) > self._timeout
+
+        subs.unregister()
+
+        if self._gesture_received:
+            '''TODO'''
+            return '''TODO'''
+        else:
+            ud.TODO = None
+            return 'aborted' # Timeouted
+
