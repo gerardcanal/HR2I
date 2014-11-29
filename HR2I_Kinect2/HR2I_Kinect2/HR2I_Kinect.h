9f91cb6a453ddffa9d83e7095f09a45e0a17a3ed
Gerard Canal <grar.knal@gmail.com>
2014-11-29 18:01:26 +0100

    Working on HR2I_Kinect2... nicing code

---
+++ HR2I_Kinect2/HR2I_Kinect2/HR2I_Kinect.h
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+
+class HR2I_Kinect2 {
+public:
+	HR2I_Kinect2();
+	~HR2I_Kinect2();
+};
---
+++ HR2I_Kinect2/HR2I_Kinect2/HR2I_Kinect2_MAIN.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include <string>
+#include <stdio.h>
+#include <iostream>
+#include "ros.h"
+
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR * argv[]) {
+
+	cout << "Hello World!" << endl;
+}
