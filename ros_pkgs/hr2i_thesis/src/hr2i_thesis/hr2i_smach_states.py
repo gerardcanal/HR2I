0bffbda1089481d0656fab2ea49d3876876fb2c4
Gerard Canal <grar.knal@gmail.com>
2014-12-16 19:49:24 +0100

    NAO move to front statemachine

--- ros_pkgs/hr2i_thesis/src/hr2i_thesis/hr2i_smach_states.py
+++ ros_pkgs/hr2i_thesis/src/hr2i_thesis/hr2i_smach_states.py
@@ -8,7 +8,9 @@ from wifibot_utils.srv import MoveToServiceRequest
 from nao_smach_utils.execute_choregraphe_behavior_state import ExecuteBehavior
 from nao_smach_utils.tts_state import SpeechFromPoolSM
 from nao_smach_utils.read_topic_state import ReadTopicState
+from nao_smach_utils.move_to_state import MoveToState
 from wifibot_smach.wifibot_goto_state import GoToPositionState
+from geometry_msgs.msg import Pose2D
 
 
 class ReleaseNAOFromWifiBotState(ExecuteBehavior):
@@ -90,22 +92,37 @@ def _Pose2D_to_str(pose):
 
 
 class WBGoToLocationNearPoint(StateMachine):
-
+    ''' Make the wifibot go to a location which is in the direction of a point but stopping at dist_to_loc m'''
     def __init__(self, dist_to_loc=0.2):
         StateMachine.__init__(self, outcomes=['succeeded', 'preempted', 'aborted'], input_keys=['ground_point'])
 
         with self:
             def prepare_req(ud):
                 # Compute the ground_point pose but displaced dist_to_loc metres in the direction of the robot
-                # To do so an equation system has been solved with the following equations (Assuming robot at 0,0:
+                # To do so an equation system has been solved with the following equations (Assuming robot at 0,0):
                 #   Line equation: y = (gy/gx) * x -- being (gx, gy) the ground point
                 #   Pitagoras: k^2 = a^2 + b^2 -- being k = dist_to_loc   . (xg, yg)
                 #   x = gx - a  --                                       /|
                 #   y = gy - b  --                                      / | <- a
                 #                                                      /__|
                 #                                                (x,y).  ^b
-                _a = math.sqrt((dist_to_loc**2) / (((-float(ud.in_ground_point.y)/ud.in_ground_point.x)**2) + 1))
-                _b = math.sqrt(dist_to_loc**2 - _a**2)
+                # (Resolution began substituing 4th equation to the first)
+                #_a = math.sqrt((dist_to_loc**2) / (((-float(ud.in_ground_point.y)/ud.in_ground_point.x)**2) + 1))
+                #_b = math.sqrt(dist_to_loc**2 - _a**2)
+
+                ###### Easier mode using thales teorem
+                ##         . (xg, yg)
+                ##    k ->/|    -- being k = dist_to_loc
+                ##(x, y)./_| <- a
+                ##      /^b|                                  x
+                ##     /   | <- A (whole) = xg                ^
+                ##    /____|                             y <__|
+                ##   .(0,0)^B = yg
+                ##########
+                # Euclidean dist between point 0 and ground_point (as robot reference point is 0,0)
+                D = math.sqrt(ud.in_ground_point.x**2+ud.in_ground_point.y**2)
+                _a = dist_to_loc*float(ud.in_ground_point.x)/D
+                _b = dist_to_loc*float(ud.in_ground_point.y)/D
                 req = MoveToServiceRequest()
                 req.pose.x = ud.in_ground_point.x - _a
                 req.pose.y = ud.in_ground_point.y - _b
@@ -124,3 +141,55 @@ class WBGoToLocationNearPoint(StateMachine):
 
             StateMachine.add('MOVE_TO_PLACE', GoToPositionState(), remapping={'request': 'move_request'},
                              transitions={'succeeded': 'succeeded', 'aborted': 'aborted'})
+
+
+class NaoGoToLocationInFront(StateMachine):
+    ''' Make the NAO go to a location which is in front of the object, and finish facing it
+    (so robot and world coordinates are even)
+    input parameters are the alpha which is the angle with respect to the start location and the point.
+    Basically:       /
+            x_world /   . (x_object, y_object) -- in NAO space
+            ^      /    |
+            |     /     |  <- K
+            |    /      |
+            |   /       . (x_object-K, y_object) -- in world space
+       .    |  /
+         .  | /
+           .|/
+<-y_world- NAO
+            ^-NAO may not be looking in the direction of x_world but in the other axis
+            The method to solve this is basically creating a vector v = (-K, 0) in world coordinates
+            and rotate it alpha degrees to have the direction in robot coordinates. Finally translate the
+            object point in the direction of the rotated v vector.
+    '''
+    def __init__(self, K=0.2):
+        StateMachine.__init__(self, outcomes=['succeeded', 'aborted'], input_keys=['alpha', 'location_point'])
+
+        def rotate_point2D(pose, alpha):
+            _pose = Pose2D()
+            _pose.x = pose.x * math.cos(alpha) - pose.y*math.sin(alpha)
+            _pose.y = pose.x * math.sin(alpha) + pose.y*math.cos(alpha)
+            return _pose
+
+        with self:
+            def prepare_pose(ud):
+                vec = Pose2D(-K, 0.0, 0.0)  # Translation vector in world space
+                rot_v = rotate_point2D(vec, -ud.in_alpha)  # Rotated point. I don't know why - but... -
+                translated_loc = Pose2D()
+                translated_loc.x = ud.in_location_point.x + rot_v.x
+                translated_loc.y = ud.in_location_point.y + rot_v.y
+                translated_loc.theta = -ud.in_alpha  # Destination point will be with the alpha rotation corrected
+                ud.out_new_loc = translated_loc
+                rospy.loginfo('--- NaoGoToLocationInFront SM -- initial position: ' + _Pose2D_to_str(ud.in_location_point) +
+                              ', translated_position' + _Pose2D_to_str(translated_loc))
+                rospy.loginfo('--- NaoGoToLocationInFront SM -- translation vector = ' + str((vec.x, vec.y)) +
+                              ', ' + str(ud.in_alpha) + 'rad rotated translation vector = ' + str((rot_v.x, rot_v.y)))
+                return 'succeeded'
+
+            StateMachine.add('PREPARE_POSE', CBState(prepare_pose, input_keys=['in_alpha', 'in_location_point'],
+                                                     output_keys=['out_new_loc'], outcomes=['succeeded']),
+                             remapping={'in_alpha': 'alpha', 'in_location_point': 'location_point', 'out_new_loc': 'target_loc'},
+                             transitions={'succeeded': 'MOVE_TO_FRONT_OBJECT'})
+
+            StateMachine.add('MOVE_TO_FRONT_OBJECT', MoveToState(), remapping={'objective': 'target_loc'},
+                             transitions={'succeeded': 'succeeded', 'aborted': 'aborted', 'preempted': 'aborted'})
